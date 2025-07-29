c1b614d48ef8beb6804081e78bc82ecde878bc18
Ayushkumar0508 <ayush.kumar@3pillarglobal.com>
2025-07-29 22:06:05 +0530

    Initial commit: Setup Playwright POM framework for Saucedemo

---
+++ data/TestData.js
@@ -0,0 +1,14 @@
+module.exports = {
+  validUser: {
+    username: 'standard_user',
+    password: 'secret_sauce'
+  },
+  invalidUser: {
+    username: 'locked_out_user',
+    password: 'wrong_pass'
+  },
+  products: [
+    { name: 'Sauce Labs Backpack', price: 29.99 },
+    { name: 'Sauce Labs Bike Light', price: 9.99 }
+  ]
+};
